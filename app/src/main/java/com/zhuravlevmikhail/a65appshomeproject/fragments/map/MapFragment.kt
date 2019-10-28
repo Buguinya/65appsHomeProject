a16c7081239fc8344f50d8f64b8fca9070dd7af6
Buguinya <b0nzhur@ya.ru>
2019-10-28 20:39:48 +0400

    Set project structure for map fragment

---
+++ app/src/main/java/com/zhuravlevmikhail/a65appshomeproject/fragments/map/MapFragment.kt
@@ -0,0 +1,12 @@
+package com.zhuravlevmikhail.a65appshomeproject.fragments.map
+
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import moxy.MvpAppCompatFragment
+
+class MapFragment : MvpAppCompatFragment(), OnMapReadyCallback {
+
+    override fun onMapReady(p0: GoogleMap?) {
+
+    }
+}
---
+++ app/src/main/java/com/zhuravlevmikhail/a65appshomeproject/fragments/map/MapPresenter.kt
@@ -0,0 +1,6 @@
+package com.zhuravlevmikhail.a65appshomeproject.fragments.map
+
+import moxy.MvpPresenter
+
+class MapPresenter : MvpPresenter<MapView>() {
+}
